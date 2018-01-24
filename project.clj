530a8e8d5cbd749f1afe5062966b44e58ff9d593
Petr Mensik <petr.mensik1@gmail.com>
2018-01-24 11:51:56 +0100

    Add original IPEX code

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject com.temify/ipex "0.1.0-SNAPSHOT"
   :description "Library for communication with IPEX VoIP call centru"
   :url "https://wwww.bizziapp.com"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [environ "1.1.0"]
+                 [clj-http "3.7.0"]
+                 [cheshire "5.8.0"]])
---
+++ src/ipex/core.clj
@@ -0,0 +1,58 @@
+(ns ipex.core
+  (:require [clj-http.client :as client]
+            [environ.core :refer [env]]
+            [taoensso.timbre :as timbre]
+            [cheshire.core :as json])
+  (:import clojure.lang.ExceptionInfo))
+
+(def ^{:doc "Configuration and credentials for the IPEX API"} api-config
+  {:url (get :ipex-url env "https://vh1107.ipex.cz/api")
+   :key (get :ipex-key env "be3c4902eb51baf3f1b97515")
+   :secret (get :ipex-secret env "cf839e7e0be0/4b1699d0/1dbb6c3a22fc")})
+
+(def ^{:doc "IPEX HTTP client configuration"} ipex-http-client-config
+  {:basic-auth [(:key api-config) (:secret api-config)]
+   :as :json
+   :content-type :json
+   :accept :json})
+
+(defn check-ipex-api-connection []
+  (and
+   (not-empty (api-config :url))
+   (not-empty (api-config :key))
+   (not-empty (api-config :secret))))
+
+(defn- get-timeouts
+  [conn socket]
+  {:conn-timeout conn
+   :socket-timeout socket})
+
+(defn make-new-call
+  [number caller-identity-mail]
+  (when (check-ipex-api-connection)
+    (try
+      (.start (Thread. (fn []
+                         (:body (client/post (str (:url api-config) "/calls?startTime=2001-01-01&dstNumber=" number)
+                                             (merge ipex-http-client-config
+                                                    (get-timeouts 30000 30000)
+                                                    {:body (json/generate-string
+                                                            {:identityType :email
+                                                             :identityValue caller-identity-mail
+                                                             :to number})}))))))
+      (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e)))
+    true))
+
+(defn get-calls-history
+  [number]
+  (try
+    (:body (client/get (str (:url api-config) "/calls?startTime=2001-01-01&dstNumber=" number)
+                       (merge ipex-http-client-config
+                              (get-timeouts 15000 15000))))
+    (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e))))
+
+(defn get-ipex-users []
+  (try
+    (:body (client/get (str (api-config :url) "/members")
+                       (merge ipex-http-client-config
+                              (get-timeouts 15000 15000))))
+    (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e))))
