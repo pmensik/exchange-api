52c3930842bfe52260e7954a312d88fbde9e6e40
Petr Mensik <petr.mensik1@gmail.com>
2018-01-29 15:17:48 +0100

    Refactor IPEX API

--- src/ipex/core.clj
+++ src/ipex/core.clj
@@ -3,12 +3,12 @@
             [environ.core :refer [env]]
             [taoensso.timbre :as timbre]
             [cheshire.core :as json])
-  (:import clojure.lang.ExceptionInfo))
+  (:import (clojure.lang ExceptionInfo)))
 
 (def ^{:doc "Configuration and credentials for the IPEX API"} api-config
-  {:url (get :ipex-url env "https://vh1107.ipex.cz/api")
-   :key (get :ipex-key env "be3c4902eb51baf3f1b97515")
-   :secret (get :ipex-secret env "cf839e7e0be0/4b1699d0/1dbb6c3a22fc")})
+  {:url (str "https://" (:ipex-url env) "/api")
+   :key (:ipex-api-key env)
+   :secret (:ipex-api-secret env)})
 
 (def ^{:doc "IPEX HTTP client configuration"} ipex-http-client-config
   {:basic-auth [(:key api-config) (:secret api-config)]
@@ -16,47 +16,45 @@
    :content-type :json
    :accept :json})
 
-(defn check-ipex-api-connection []
-  (and
-   (not-empty (api-config :url))
-   (not-empty (api-config :key))
-   (not-empty (api-config :secret))))
+(def ^{:doc "Call timeout"} call-timeout-settings
+  {:conn-timeout 300000
+   :socket-timeout 30000})
 
-(defn- get-timeouts
-  [conn socket]
-  {:conn-timeout conn
-   :socket-timeout socket})
+(defn get-token
+  "Returns auth token for further API communication. Token expires in 60 minutes"
+  [user-email]
+  (:body (client/post (str (:url api-config) "/auth/token")
+                      (merge ipex-http-client-config
+                             call-timeout-settings
+                             {:form-params {:identityType "email"
+                                            :identityValue user-email}}))))
 
-(defn make-new-call
-  "Issues new call to a number with provided user email"
+(defn make-call
+  "Issues new call to a number authenticated with user email"
   [number user-email]
-  (when (check-ipex-api-connection)
-    (try
-      (.start (Thread. (fn []
-                         (:body (client/post (str (:url api-config) "/calls?startTime=2001-01-01&dstNumber=" number)
-                                             (merge ipex-http-client-config
-                                                    (get-timeouts 30000 30000)
-                                                    {:body (json/generate-string
-                                                            {:identityType :email
-                                                             :identityValue user-email
-                                                             :to number})}))))))
-      (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e)))
-    true))
+  (:body (client/post (str (:url api-config) "/calls")
+                      (merge ipex-http-client-config
+                             call-timeout-settings
+                             {:form-params {:identityType "login"
+                                            :identityValue user-email
+                                            :to number}}))))
+       ; (catch ExceptionInfo ex
+       ;   (ex-info "Problem connection to the IPEX" ex))))
 
-(defn get-calls-history
+(defn get-call-history
   "Returns call history for given number"
   [number]
-  (try
-    (:body (client/get (str (:url api-config) "/calls?startTime=2001-01-01&dstNumber=" number)
-                       (merge ipex-http-client-config
-                              (get-timeouts 15000 15000))))
-    (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e))))
+  (try (:body (client/get (str (:url api-config) "/calls")
+                          (assoc ipex-http-client-config
+                                 :query-params {:dstNumber number
+                                                :startTime "2001-01-01"})));TODO remove time
+       (catch ExceptionInfo ex
+         (ex-info "Problem connection to the IPEX" ex))))
 
-(defn get-ipex-users 
+(defn get-ipex-users
   "Returns al IPEX users for provided credentials"
   []
-  (try
-    (:body (client/get (str (api-config :url) "/members")
-                       (merge ipex-http-client-config
-                              (get-timeouts 15000 15000))))
-    (catch ExceptionInfo e (timbre/error "Problem connecting to the IPEX" e))))
+  (try (:body (client/get (str (api-config :url) "/members")
+                          ipex-http-client-config))
+       (catch ExceptionInfo ex
+         (ex-info "Problem connection to the IPEX" ex))))
