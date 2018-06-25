6ba4105ce34134a2940713bf423ea5c907c4a481
Petr Mensik <petr.mensik1@gmail.com>
2018-06-25 18:59:27 +0200

    Update to new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.temify/pdf-utils "0.0.1-SNAPSHOT"
+(defproject com.temify/pdf-utils "0.0.2-SNAPSHOT"
   :description "Library for easier filling of PDF forms across all Biziapp instances"
   :url "https://wwww.bizziapp.com"
 
