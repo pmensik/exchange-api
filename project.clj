1a4be34a1cae40a68f027e9e36f0e194d60687e9
Petr Mensik <petr.mensik1@gmail.com>
2018-07-27 14:50:11 +0200

    Promote to version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.temify/ews-clojure-api "0.0.3"
+(defproject com.temify/ews-clojure-api "0.0.4"
   :description "Utility library for accessing Microsoft Exchange"
   :url "https://wwww.bizziapp.com"
 
